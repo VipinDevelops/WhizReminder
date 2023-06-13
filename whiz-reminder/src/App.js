5556672618d75338e0176c54b9b0a2b90935a221
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-13 17:32:35 +0530

    style: :arrow_up: add bootstrap and font awesome icon

--- whiz-reminder/src/App.js
+++ whiz-reminder/src/App.js
@@ -1,3 +1,7 @@
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import './App.css';
 import ReminderList from './components/ReminderList';
 
--- whiz-reminder/src/index.js
+++ whiz-reminder/src/index.js
@@ -1,6 +1,4 @@
-//Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
