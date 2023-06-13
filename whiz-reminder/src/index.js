1760aafef52e37c5d9fceb6714527bc445e32447
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-13 14:04:06 +0530

    build(setup): :heavy_plus_sign: npm install bootstrap reactstrap

--- whiz-reminder/src/index.js
+++ whiz-reminder/src/index.js
@@ -1,3 +1,6 @@
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
