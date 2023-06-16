43152afa184426aa988aadaeb331ae29bdf9be0b
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-16 21:45:46 +0530

    feat(reminder): :sparkles: add the reminder feature

--- whiz-reminder/backend/index.js
+++ whiz-reminder/backend/index.js
@@ -17,53 +17,85 @@ dotenv.config();
 //connect to database
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+setInterval(async () => {
+  try {
+    const reminderList = await Reminder.find({});
+
+    reminderList.forEach(async (reminder) => {
+      if (!reminder.isReminded) {
+        const now = new Date();
+        if (new Date(reminder.remindAt) - now < 0) {
+          await Reminder.findByIdAndUpdate(reminder.id, { isReminded: true });
+
+          const accountSid = process.env.ACCOUNT_SID;
+          const authToken = process.env.AUTH_TOKEN;
+          const client = require("twilio")(accountSid, authToken);
+
+          const date = new Date(reminder.remindAt);
+          const time = date.toLocaleString("en-US", { timezone: "Asia/Kolkata" });
+
+          console.log(`Reminder for ${reminder.reminderName}`)
+          await client.messages.create({
+            body: `Hey Vipin, quick reminder! Don't forget ${reminder.reminderName} - ${reminder.reminderDescription} at ${time}.
+              `,
+            from: "whatsapp:+14155238886",
+            to: `whatsapp:${process.env.MY_PHONE_NUMBER}`,
+          });
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}, 1000);
 
 // API endpoints
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.get('/api/Getallreminder', async (req, res) => {
-    try {
-        const allReminder = await Reminder.find();
-        res.json(allReminder);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-    }
+  try {
+    const allReminder = await Reminder.find({ isReminded: false });
+    res.json(allReminder);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.post('/api/createreminder', async (req, res) => {
-    try {
-        const { reminderName, reminderDescription, remindAt } = req.body;
-        const newReminder = new Reminder({
-            reminderName,
-            reminderDescription,
-            remindAt,
-            isReminded: false,
-        });
-        await newReminder.save();
-        const allReminder = await Reminder.find();
-        res.json(allReminder);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-    }
+  try {
+    const { reminderName, reminderDescription, remindAt } = req.body;
+    const newReminder = new Reminder({
+      reminderName,
+      reminderDescription,
+      remindAt,
+      isReminded: false,
+    });
+    await newReminder.save();
+    const allReminder = await Reminder.find();
+    res.json(allReminder);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.delete('/api/deleteReminder', async (req, res) => {
-    try {
-        const { id } = req.body;
-        await Reminder.findByIdAndDelete(id);
-        const allReminder = await Reminder.find();
-        res.json(allReminder);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-    }
+  try {
+    const { id } = req.body;
+    await Reminder.findByIdAndDelete(id);
+    const allReminder = await Reminder.find();
+    res.json(allReminder);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
