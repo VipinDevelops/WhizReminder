8e1817f264910d765b474925c2508bccab219de5
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-17 10:40:47 +0530

    refactor(component): :wrench: add config file for base URL

--- whiz-reminder/frontend/src/components/ReminderList.js
+++ whiz-reminder/frontend/src/components/ReminderList.js
@@ -4,13 +4,16 @@ import CreateReminder from '../modals/CreateReminder';
 import Card from './Card';
 import Header from './Header'
 import axios from 'axios';
+import API_URL from '../config'
 
 const ReminderList = (props) => {
   const [modal, setModal] = useState(false);
   const [reminderList, setReminderList] = useState([]);
 
+  const BASE_URL = API_URL;
+  
   useEffect(() => {
-    axios.get('http://localhost:4000/api/Getallreminder').then((res) => {
+    axios.get(`${BASE_URL}/Getallreminder`).then((res) => {
       console.log(res.data)
       setReminderList(res.data)
     });
@@ -19,7 +22,7 @@ const ReminderList = (props) => {
   }, []);
 
   const deleteTask = (id) => {
-    axios.delete('http://localhost:4000/api/deleteReminder', { data: { id: id } }).then((res) => {
+    axios.delete(`${BASE_URL}/deleteReminder`, { data: { id: id } }).then((res) => {
       console.log(res.data)
       setReminderList(res.data)
     });
@@ -29,7 +32,7 @@ const ReminderList = (props) => {
 
   const savetask = (task) => {
     console.log(task)
-    axios.post('http://localhost:4000/api/createreminder', task).then((res) => {
+    axios.post(`${BASE_URL}/createreminder`, task).then((res) => {
       console.log(res.data)
       setReminderList(res.data)
     });
---
+++ whiz-reminder/frontend/src/config.js
@@ -0,0 +1,3 @@
+const API_URL = 'http://localhost:4000/api';
+
+export default API_URL;
