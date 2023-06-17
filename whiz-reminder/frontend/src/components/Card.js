51a7bf51cb66ab7a42d11dc410a15ea5d9cf3091
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-17 10:41:22 +0530

    fix(modal): :pencil2: fix minor waring errors

--- whiz-reminder/frontend/src/components/Card.js
+++ whiz-reminder/frontend/src/components/Card.js
@@ -34,20 +34,19 @@ const Card = ({ taskObj, index, deleteTask, id }) => {
 
 
     return (
-
-        <div class="card-wrapper mr-5">
-            <div class="card-top" style={{ "background-color": colors[index % 5].primaryColor }}></div>
-            <div class="task-holder">
-                <span class="card-header" style={{ "background-color": colors[index % 5].secondaryColor, "border-radius": "10px" }}>{taskObj.reminderName}</span>
+        <div className="card-wrapper mr-5">
+            <div className="card-top" style={{ backgroundColor: colors[index % 5].primaryColor }}></div>
+            <div className="task-holder">
+                <span className="card-header" style={{ backgroundColor: colors[index % 5].secondaryColor, borderRadius: "10px" }}>{taskObj.reminderName}</span>
                 <p className="mt-3">{taskObj.reminderDescription}</p>
                 <p className="mt-3">{date.toLocaleString("en-US", { timezone: "Asia/Kolkata" })}</p>
-                <div style={{ "position": "absolute", "right": "20px", "bottom": "20px" }}>
-                    <i class="fas fa-trash-alt" style={{ "color": colors[index % 5].primaryColor, "cursor": "pointer" }} onClick={handleDelete}></i>
+                <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
+                    <i className="fas fa-trash-alt" style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }} onClick={handleDelete}></i>
                 </div>
             </div>
-
         </div>
     );
+
 };
 
 export default Card;
