6554f3a1b0c91d29d12a8c4f3136e6a34202edca
imvipindev <vipinchaudhary1809@gmail.com>
2023-06-17 11:04:43 +0530

    feat(Database): :card_file_box: Added mongodb Cloud Database

--- whiz-reminder/backend/db.js
+++ whiz-reminder/backend/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+dotenv.config();
 
 const connectDB = async () => {
+    const MONGOURI = process.env.MONGOURI;
     try {
-        const connect = await mongoose.connect(`mongodb://localhost:27017/WhizReminder`, {
+        const connect = await mongoose.connect(MONGOURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
